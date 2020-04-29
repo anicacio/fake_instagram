f872f2f383056171f69cfc29d69ba5412562ab32
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-04-29 14:48:11 -0300

    Add Posts.js e Comentario.js

---
+++ models/Comentario.js
@@ -0,0 +1,30 @@
+const Comentario = (sequelize, Datatypes)=>{
+
+    return sequelize.define(
+        'Comentario',{
+            id:{
+                type: Datatypes.INTEGER,
+                primaryKey:true,
+                autoIncrement:true,
+                allowNull:false
+            },
+            texto:{
+                type:Datatypes.STRING,
+                allowNull:false,
+            },
+            usuarios_id:{
+                type:Datatypes.INTEGER,
+                allowNull:false,
+            },
+            posts_id:{
+                type:Datatypes.INTEGER,
+                allowNull:false,
+            }
+        },
+        {
+            tableName:'comentarios',
+            timestamps:false
+        }
+    )};
+
+    module.exports = Comentario;
---
+++ models/Post.js
@@ -0,0 +1,34 @@
+const Post = (sequelize, Datatypes)=>{
+
+    return sequelize.define(
+        'Post',{
+            id:{
+                type: Datatypes.INTEGER,
+                primaryKey:true,
+                autoIncrement:true,
+                allowNull:false
+            },
+            texto:{
+                type:Datatypes.STRING,
+                allowNull:false,
+            },
+            img:{
+                type:Datatypes.STRING,
+                allowNull:false,
+            },
+            usuarios_id:{
+                type:Datatypes.INTEGER,
+                allowNull:false,
+            },
+            n_likes:{
+                type:Datatypes.INTEGER,
+                allowNull:false,
+            }
+        },
+        {
+            tableName:'posts',
+            timestamps:false
+        }
+    )};
+
+    module.exports = Post;
